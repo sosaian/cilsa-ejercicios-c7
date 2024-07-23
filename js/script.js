0bc30e221ecbd41dc5260b802359133003951080
Gabriel <gabiabaldee@gmail.com>
2024-07-23 19:15:15 -0300

    Soulución de conflictos entre Ramas Anto-Gabi

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const secciones = document.querySelectorAll(".seccion");
-
-    // secciones.forEach(function(seccion) {
-    //     seccion.addEventListener("click", () => {
-    //         seccion.setAttribute("data-visible", "false");
-    //     });
-    // });
-});
