9cc70a557d25667ed0fd4afa122dd35248d1625c
Gabriel <gabiabaldee@gmail.com>
2024-07-22 22:12:32 -0300

    Prueba merge con rama de trabajo remota

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const secciones = document.querySelectorAll(".seccion");
+
+    // secciones.forEach(function(seccion) {
+    //     seccion.addEventListener("click", () => {
+    //         seccion.setAttribute("data-visible", "false");
+    //     });
+    // });
+});
